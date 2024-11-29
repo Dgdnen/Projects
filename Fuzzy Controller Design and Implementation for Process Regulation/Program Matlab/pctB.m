0f1ce0624a2bc0f1d4ec3eecb9422fef74f16040
Bogdan Patrania <75747858+Dgdnen@users.noreply.github.com>
2024-11-30 00:43:02 +0200

    Add files via upload

--- Fuzzy Controller Design and Implementation for Process Regulation/Program Matlab/pctB.m
+++ Fuzzy Controller Design and Implementation for Process Regulation/Program Matlab/pctB.m
@@ -1,1 +1,14 @@
+fis = readfis("fuzzyMamdani_pct1_proj.fis");
 
+u=zeros(5,11);
+i=1;
+for e = -1.0:0.2:1.0
+    j=1;
+    for de = -1.0:0.5:1.0 
+        inputs = [e de];                   
+        u(j,i) = evalfis(fis,inputs);         
+        j=j+1;
+    end
+    i=i+1;
+end
+u
